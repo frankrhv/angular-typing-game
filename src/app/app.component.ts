69c7c3dddd0c53ac10ba0f393ba87ff12bb4230d
Frank Huaylinos Velasquez <fhuaylinosv@uni.pe>
2022-04-14 23:43:42 -0500

    typing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(target: EventTarget) {
+    const value = (target as HTMLInputElement).value;
+    this.enteredText = value;
+  }
+
+  compare(randomletter: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return enteredLetter === randomletter ? 'correct' : 'incorrect';
+  }
 }
